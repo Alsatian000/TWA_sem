f31e9bcf29511f089b144fa131115981de5c9135
Tomáš Smazal <wolfsburrow@gmail.com>
2014-05-10 18:52:49 +0200

    002

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="author" content="Tomas Smazal"/>
+<meta name="robots" content="index"/>
+
+<script type="text/javascript" src="js/page.js"></script>
+
+<title>Matrika</title>
+
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+</head>
+
+<body  alink="#000000" vlink="#000000" link="#000000">
+
+
+
+
+
+</body>
+</html>
