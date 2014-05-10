6aa0385fa854e78f1d31df01615f5375d633e3d8
Michael Dostál <renkor.md@gmail.com>
2014-05-10 19:08:51 +0200

    css js

---
+++ form-inputs.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+		<meta http-equiv="keywords" content="" />
+		<meta http-equiv="description" content="" />
+		<meta name="robots" content="index"/>
+		<meta name="author" content="Tomas Smazal"/>
+		<title>TWA - tom forms</title>
+		<!--<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">-->
+      	<link rel="stylesheet" type="text/css" href="css/inputs.css" />
+		<script type="text/javascript" async="" src="http://utils.similagroup.com/countryscript/v3.js"></script>
+		
+	<!--	<script type="text/javascript" src="js/page.js"></script>-->
+	</head>
+
+	<body alink="#000000" vlink="#000000" link="#000000">
+
+	</body>
+</html>
